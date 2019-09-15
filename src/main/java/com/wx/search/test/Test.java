2669fcfcb0ada6133ce5a4f926d0ec20ca9cd71b
wangxu <wangxud1991@live.com>
2019-09-15 13:51:05 +0800

    2019新增

---
+++ src/main/java/com/wx/search/test/Test.java
@@ -0,0 +1,14 @@
+package com.wx.search.test;
+
+/**
+ * @ClassName Test
+ * @Description
+ * @Author wx
+ * @Date 2019/9/15 1:50 PM
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
